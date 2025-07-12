96e5c15ebf4edc66d6ac18d418d347eacc8e70ee
Isenewo Oluwaseyi Ephraim <isenewoephr2012@gmail.com>
2025-07-13 00:37:18 +0100

    feat: Initialized django in starkinsure-risk-analytics

---
+++ blockchain_analytics/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlockchainAnalyticsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blockchain_analytics'
---
+++ kyc_processor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KycProcessorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kyc_processor'
